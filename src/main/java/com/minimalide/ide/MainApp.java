4164f56bcbbdb6150f2eeb765d942f3e9c0ab4fb
Felipe Muller <fmuller@edu.univali.br>
2026-04-13 18:53:59 -0300

    Update Gals

--- src/main/java/com/minimalide/ide/MainApp.java
+++ src/main/java/com/minimalide/ide/MainApp.java
@@ -9,12 +9,20 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        javafx.scene.text.Font.loadFont(getClass().getResourceAsStream("/fonts/JetBrainsMono-Regular.ttf"), 16);
+        javafx.scene.text.Font.loadFont(
+            getClass().getResourceAsStream("/fonts/JetBrainsMono-Regular.ttf"),
+            16
+        );
 
         CompilerController controller = new CompilerController();
         Scene scene = new Scene(controller.buildLayout(stage), 960, 680);
-        scene.getStylesheets().add(getClass().getResource("/com/minimalide/style.css").toExternalForm());
-
+        scene
+            .getStylesheets()
+            .add(
+                getClass()
+                    .getResource("/com/minimalide/style.css")
+                    .toExternalForm()
+            );
 
         scene.setFill(javafx.scene.paint.Color.web("#0e0e0e"));
         stage.setTitle("MinimalIDE");
